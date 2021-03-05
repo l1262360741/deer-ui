3ad9ce0165ec9007035aeb0b837a46c94169e9f0
liushizhao <1262360741@qq.com>
2021-03-05 09:33:02 +0800

    完善aside

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,10 @@ import { ref, provide } from "vue";
 export default {
   name: "App",
   setup() {
-    const menuVisible = ref(false);
-    provide('menuVisible',menuVisible)
+    const width = document.documentElement.clientWidth;
+    console.log(width);
+    const menuVisible = ref(width <= 300 ? false : true);
+    provide("menuVisible", menuVisible);
   }
 };
 </script>
